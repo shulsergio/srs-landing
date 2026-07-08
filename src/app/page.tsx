2f69027c497d5bb26eac6f003354711038866a15
Sergey Shulga <shul.sergio@gmail.com>
2026-07-08 15:18:33 +0300

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+// import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.intro}>
+          <h1>SRS </h1>
+        </div>
+      </main>
+    </div>
+  );
+}
